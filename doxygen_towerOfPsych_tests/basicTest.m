b05d03e0859a7362c8f9f99b1c3d62e9078102c1
TheDerp <40202026+DeepFriedDerp@users.noreply.github.com>
2019-08-14 10:04:59 -0400

    derp
    derp

---
+++ doxygen_towerOfPsych_tests/basicTest.m
@@ -0,0 +1,15 @@
+%%%
+%
+%@file basicTest.m
+%@brief This is a test of the matlab to c++ doxygen filter
+%@details The documentation for tower of psych sucks the big balls. 
+% it doesnt really show you where to go to download just the filter, and
+% the rest of the package is of unknown functionality from 2012.
+%
+
+%%%
+%@brief a function that does nothing
+% @param[in] derp A dummy input parameter
+%
+function blerp(derp)
+end
