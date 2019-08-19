f6ac3175aa7678bf1dce075b5674f3199ac747f7
TheDerp <40202026+DeepFriedDerp@users.noreply.github.com>
2019-08-19 10:17:58 -0400

    Create TransportTheorum_1.m
    Created a function to perform the 1st transport theorum (A frame time derivatives on B-frame vectors).

---
+++ Basics_Dynamics/TransportTheorum_1.m
@@ -0,0 +1,38 @@
+function [Addt_q_B] = TransportTheorum_1(vector_q_B, A_omega_B_B)
+%TransportTheorum_1 performs a single time derivative in the A frame on the
+%vector q, which is defined in the B frame
+%   
+%   FORMAT : [vector_dq_B] = TransportTheorum_1(vector_q_B, A_omega_B_B)
+%
+%   ARGUMENTS :
+%       OUTPUT vector_dq_B : The symbolic column 3-vector representing the
+%       A frame time derivative of original 3-vector q.
+%
+%       INPUT vector_q_B : The symbolic column 3-vector q (defined in the B
+%       frame) to be differentiated
+%
+%       INPUT A_omega_B_B : The symbolic column 3-vector representing the
+%       angular velocity of frame B with respect to frame A, expressed in
+%       the B frame
+
+%validate that the number of arguments is correct
+nargoutchk(1,1);
+narginchk(2,2);
+
+%setup accepted input argument classes, and any required values and
+%attributes for each argument
+inputClass = {'sym'};
+inputAttributes = {'column'};
+
+%validate each argument for proper type and attributes
+validateattributes(vector_q_B,inputClass,inputAttributes);
+validateattributes(A_omega_B_B,inputClass,inputAttributes);
+
+fprintf('Transporting The Theorums all over the place...\n')
+
+% Actually perform the 1st Transport Theorum
+Addt_q_B = diff(vector_q_B) + cross(A_omega_B_B,vector_q_B);
+
+fprintf('Theorums have been transported!\n')
+
+end
