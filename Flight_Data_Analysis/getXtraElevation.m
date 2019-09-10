0bc247842ee291fef016de7f79ff259f0c4312a1
DeepFriedDerp <agmotes@ncsu.edu>
2019-09-10 15:41:01 -0400

    Create getXtraElevation.m

---
+++ Flight_Data_Analysis/getXtraElevation.m
@@ -0,0 +1,27 @@
+function getXtraElevation(FileName)
+    if exist(FileName)
+        if ~exist('CALIB')
+            mkdir CALIB;
+        end
+        cd CALIB;
+        if ~exist('ALTIMETER')
+            mkdir ALTIMETER;
+        end
+        cd ..;
+    else
+        error("FILENAME DOES NOT EXIST");
+    end
+    
+    inputFID = fopen(FileName,'r');
+    
+    line = fgetl(inputFID);
+    gpsElev = 0;
+    i = 0;
+    while ischar(line)
+        i = i + 1;
+        AltElev(i,1) = str2double(extractAfter(extractBefore(line,':'),','));
+        line = fgetl(inputFID);
+    end
+    
+    avgElev = mean(AltElev);
+end
