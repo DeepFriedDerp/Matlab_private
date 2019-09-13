d5fe07824c48fc857fd297083a174e31e6120d89
TheDerp <40202026+DeepFriedDerp@users.noreply.github.com>
2019-09-12 21:04:30 -0400

    yep

--- Flight_Data_Analysis/EulerQuaternionFinder/Twelve_OcB_RotationFunctions/rotationFunction_yxz.m
+++ Flight_Data_Analysis/EulerQuaternionFinder/Twelve_OcB_RotationFunctions/rotationFunction_yxz.m
@@ -1,7 +1,7 @@
 function [xO,yO,zO] = rotationFunction_yxz(ph,th,ps,xB,yB,zB)
 
-xO = xB*(cos(ps)*cos(th) - sin(ph)*sin(ps)*sin(th)) + zB*(cos(ps)*sin(th) + cos(th)*sin(ph)*sin(ps)) - yB*cos(ph)*sin(ps);
-yO = xB*(cos(th)*sin(ps) + cos(ps)*sin(ph)*sin(th)) + zB*(sin(ps)*sin(th) - cos(ps)*cos(th)*sin(ph)) + yB*cos(ph)*cos(ps);
-zO = yB*sin(ph) + zB*cos(ph)*cos(th) - xB*cos(ph)*sin(th);
+xO = xB*(cos(ps)*cos(th) + sin(ph)*sin(ps)*sin(th)) - yB*(cos(ps)*sin(th) - cos(th)*sin(ph)*sin(ps)) + zB*cos(ph)*sin(ps);
+yO = yB*cos(ph)*cos(th) - zB*sin(ph) + xB*cos(ph)*sin(th);
+zO = yB*(sin(ps)*sin(th) + cos(ps)*cos(th)*sin(ph)) - xB*(cos(th)*sin(ps) - cos(ps)*sin(ph)*sin(th)) + zB*cos(ph)*cos(ps);
 
 end
