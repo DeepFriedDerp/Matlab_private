2d80befd66d66f6ed82ee3c498b4fee1c8c77a87
TheDerp <40202026+DeepFriedDerp@users.noreply.github.com>
2019-09-22 19:08:55 -0400

    Update gps3VectorPos.m
    Updated with the option to add in high-accuracy elevation data.

--- Flight_Data_Analysis/gps3VectorPos.m
+++ Flight_Data_Analysis/gps3VectorPos.m
@@ -1,54 +1,119 @@
-function gps3VectorPos(InputFile)
-    if exist(InputFile)
-        if contains(InputFile,'.TXT')
-            input_substring = extractBefore(InputFile,'.TXT');
-        else
-            input_substring = extractBefore(InputFile,'.txt');
-        end
-            OutputFile = append(input_substring,"_GPS3VectPos.txt");
-        
+function gps3VectorPos(InputFile,OtherElevFile)
+    if ~exist(InputFile)
+        error("inputFile does not exist")
+    end
+    if contains(InputFile,'.TXT')
+        input_substring = extractBefore(InputFile,'.TXT');
+    else
+        input_substring = extractBefore(InputFile,'.txt');
+    end
+    OutputFile = append(input_substring,"_GPS3VectPos.txt");
+
+    if nargin > 1
+        OtherElevFID = fopen(OtherElevFile,'r');
         inputFID = fopen(InputFile,'r');
         outputFID = fopen(OutputFile,'w');
-    end
-    
-    line = '11';
-    j = 0;
-    commonTime = "0";
-    latString = "0";
-    lonString = "0";
-    elevString = "0";
-    latDeg = "0";
-    latMinute = "0";
-    lonDeg = "0";
-    lonMinute = "0";
-    latNum = 0;
-    lonNum = 0;
-    while ischar(line)
-        j = j+1;
-        line = fgetl(inputFID);
-        if ischar(line) && contains(line,'GGA')
-            commaIndex = strfind(line,",");
-            commonTime = extractAfter(extractBefore(line,commaIndex(1,2)),"GGA,");
-            latString = extractAfter(extractBefore(line,commaIndex(1,4)),commaIndex(1,2));
-            lonString = extractAfter(extractBefore(line,commaIndex(1,6)),commaIndex(1,4));
-            elevString = extractAfter(extractBefore(line,commaIndex(1,10)),commaIndex(1,9));
+        
+        workingSetInput = [0 0 0 0];
+        workingSetElev = [0 0];
+        
+        inputLine = fgetl(inputFID);
+        ElevLine = fgetl(OtherElevFID);
+        
+        i = 0;
+        j = 0;
+        while ischar(inputLine) || ischar(ElevLine)
+            i = i+1;
             
-            latDeg = extractBefore(latString,3);
-            latMinute = extractAfter(extractBefore(latString,","),2);
-            lonDeg = extractBefore(lonString,4);
-            lonMinute = extractAfter(extractBefore(lonString,","),3);
-            latNum = str2double(latDeg) + (str2double(latMinute)/60);
-            lonNum =  str2double(lonDeg) + (str2double(lonMinute)/60);
-            
-            if contains(latString,"E")
-                latNum = (-1)*latNum;
+            if ischar(inputLine) && contains(inputLine,'GGA')
+                j = j + 1;
+                commaIndex = strfind(inputLine,",");
+                commonTime = str2double(extractAfter(extractBefore(inputLine,commaIndex(1,2)),"GGA,"));
+                latString = extractAfter(extractBefore(inputLine,commaIndex(1,4)),commaIndex(1,2));
+                lonString = extractAfter(extractBefore(inputLine,commaIndex(1,6)),commaIndex(1,4));
+                elev = str2double(extractAfter(extractBefore(inputLine,commaIndex(1,10)),commaIndex(1,9)));
+                
+                latDeg = extractBefore(latString,3);
+                latMinute = extractAfter(extractBefore(latString,","),2);
+                lonDeg = extractBefore(lonString,4);
+                lonMinute = extractAfter(extractBefore(lonString,","),3);
+                latNum = str2double(latDeg) + (str2double(latMinute)/60);
+                lonNum =  str2double(lonDeg) + (str2double(lonMinute)/60);
+
+                if contains(latString,"E")
+                    latNum = (-1)*latNum;
+                end
+                if contains(lonString,'W')
+                    lonNum = (-1)*lonNum;
+                end
+                
+                workingSetInput(j,:) = [commonTime latNum lonNum elev];
+
+                %fprintf(outputFID,"%s,%.6f,%.6f,%s\n",commonTime,latNum,lonNum,elevString);
             end
-            if contains(lonString,'W')
-                lonNum = (-1)*lonNum;
-            end
             
-           
-            fprintf(outputFID,"%s,%.6f,%.6f,%s\n",commonTime,latNum,lonNum,elevString);
+            commonTime = str2double(extractBefore(ElevLine,','));
+            altElev = str2double(extractAfter(ElevLine,','));
+            
+            workingSetElev(i,:) = [commonTime altElev];
+            
+            inputLine = fgetl(inputFID);
+            ElevLine = fgetl(OtherElevFID);
+        end
+        
+        elevTimeLo = workingSetElev(1,1);
+        elevTimeHi = workingSetElev(size(workingSetElev,1),1);
+        
+        h = 1;
+        for k = 1:j
+            inputTime = workingSetInput(k,1);
+            if inputTime < elevTimeLo || inputTime > elevTimeHi
+                fprintf(outputFID,"%.3f,%.6f,%.6f,%.1f\n",workingSetInput(k,1),workingSetInput(k,2),workingSetInput(k,3),workingSetInput(k,4));
+            else
+                h = 1;
+                while workingSetElev(h,1) < inputTime
+                    h = h + 1;
+                end
+                h = h - 1;
+                avgElev = mean(workingSetElev(h:h+1,2));
+                fprintf(outputFID,"%.3f,%.6f,%.6f,%.3f\n",workingSetInput(k,1),workingSetInput(k,2),workingSetInput(k,3),avgElev);
+            end
+        end
+        fclose(OtherElevFID);
+    else
+        inputFID = fopen(InputFile,'r');
+        outputFID = fopen(OutputFile,'w');
+        
+        inputLine = fgetl(inputFID);
+        
+        i = 0;
+        while ischar(inputLine) 
+            i = i+1;
+            line = fgetl(inputFID);
+            if ischar(inputLine) && contains(inputLine,'GGA')
+                commaIndex = strfind(inputLine,",");
+                commonTime = extractAfter(extractBefore(inputLine,commaIndex(1,2)),"GGA,");
+                latString = extractAfter(extractBefore(inputLine,commaIndex(1,4)),commaIndex(1,2));
+                lonString = extractAfter(extractBefore(inputLine,commaIndex(1,6)),commaIndex(1,4));
+                elevString = extractAfter(extractBefore(inputLine,commaIndex(1,10)),commaIndex(1,9));
+
+                latDeg = extractBefore(latString,3);
+                latMinute = extractAfter(extractBefore(latString,","),2);
+                lonDeg = extractBefore(lonString,4);
+                lonMinute = extractAfter(extractBefore(lonString,","),3);
+                latNum = str2double(latDeg) + (str2double(latMinute)/60);
+                lonNum =  str2double(lonDeg) + (str2double(lonMinute)/60);
+
+                if contains(latString,"E")
+                    latNum = (-1)*latNum;
+                end
+                if contains(lonString,'W')
+                    lonNum = (-1)*lonNum;
+                end
+
+                fprintf(outputFID,"%s,%.6f,%.6f,%s\n",commonTime,latNum,lonNum,elevString);
+                inputLine = fgetl(inputFID);
+            end
         end
     end
     fclose(inputFID);
